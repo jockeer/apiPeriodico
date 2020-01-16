a3312eef376e4472ac20ee2ade30cc6e0469a2ee
Daniel Gorianz <aalbaafikaa@gmail.com>
2020-01-16 13:11:46 -0400

    Se modifico el ingreso a lalos items del menu

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ var options = {
 
 var pgp = require('pg-promise')(options);
 
-var connectionString = 'postgres://khpyzscwcpngyb:c4efda34ee0e93f087d50aba3055b56d73d840bf30c02fdb2d7066132ef695a4@ec2-174-129-33-147.compute-1.amazonaws.com:5432/dbcouaatp3hrtk';
+var connectionString = 'postgres://postgres:posgres@localhost:5432/ZonaNuevoNorte';
 // var connectionString = 'postgres://khpyzscwcpngyb:c4efda34ee0e93f087d50aba3055b56d73d840bf30c02fdb2d7066132ef695a4@ec2-174-129-33-147.compute-1.amazonaws.com:5432/dbcouaatp3hrtk';
 var db = pgp(connectionString);
 
@@ -49,6 +49,214 @@ function obtenerUltimasCinco(req, res, next) {
 }
 function obtenerOtrasNotas(req, res, next) {
     db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC LIMIT 5) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoMontero(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 1 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasMontero(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=1 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoLocalidades(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 2 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasLocalidades(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=2 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoSantaCruz(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 3 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasSantaCruz(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=3 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoSeguridad(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 4 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasSeguridad(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=4 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoDeportes(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 5 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasDeportes(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=5 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoSociales(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 6 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasSociales(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=6 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoNacional(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 7 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasNacional(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=7 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerUltimasCincoInternacional(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n,subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria = 8 ORDER BY idnoticia DESC LIMIT 5`)
+        .then(function(data) {
+            res.status(200).json({
+                status: 'success',
+                data: data,
+                message: 'Noticia Obtenido'
+            });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+function obtenerOtrasNotasInternacional(req, res, next) {
+    db.any(`SELECT n.idnoticia,n.titulo,n.subtitulo,n.texto,n.fotografia,n.frase,n.fecha,ca.nombrecategoria from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) WHERE n.idcategoria=8 and idnoticia not IN (SELECT n.idnoticia from Noticia n INNER JOIN categoria ca on(n.idcategoria = ca.idcategoria) ORDER BY idnoticia DESC) ORDER BY idnoticia DESC Limit 10`)
         .then(function(data) {
             res.status(200).json({
                 status: 'success',
@@ -105,12 +313,62 @@ function IngresarComentario(req, res, next) {
         });
 }
 
+function InsertarEmpresa(req, res, next) {
+    db.none('INSERT into empresa(nombreEmpresa)VALUES(${nombreEmpresa})',
+            req.body)
+        .then(function() {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Registrado Correctamente'
+                });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+function InsertarPublicidad(req, res, next) {
+    db.none('insert into publicidad(categoriaPublicidad,foto,estado,tipo,idEmpresa)VALUES(${categoriaPublicidad},${foto},${estado},${tipo},${idEmpresa})',
+            req.body)
+        .then(function() {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Registrado Correctamente'
+                });
+        })
+        .catch(function(err) {
+            return next(err);
+        });
+}
+
+
+
 module.exports = {
     obtenerUsuario,
     obtenerUltimasCinco,
     obtenerOtrasNotas,
+    obtenerUltimasCincoMontero,
+    obtenerOtrasNotasMontero,
+    obtenerUltimasCincoLocalidades,
+    obtenerOtrasNotasLocalidades,
+    obtenerUltimasCincoSantaCruz,
+    obtenerOtrasNotasSantaCruz,
+    obtenerUltimasCincoSeguridad,
+    obtenerOtrasNotasSeguridad,
+    obtenerUltimasCincoDeportes,
+    obtenerOtrasNotasDeportes,
+    obtenerUltimasCincoSociales,
+    obtenerOtrasNotasSociales,
+    obtenerUltimasCincoNacional,
+    obtenerOtrasNotasNacional,
+    obtenerUltimasCincoInternacional,
+    obtenerOtrasNotasInternacional,
     obtenerCometarios,
     IngresarNota,
-    IngresarComentario
+    IngresarComentario,
+    InsertarEmpresa,
+    InsertarPublicidad
 };
 
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,26 @@ router.get('/api/obtenerUsuario/:usuario/:pass', db.obtenerUsuario);
 router.get('/api/obtenerCometarios/:idnoticia', db.obtenerCometarios);
 router.get('/api/obtenerUltimasCinco', db.obtenerUltimasCinco);
 router.get('/api/obtenerOtrasNotas', db.obtenerOtrasNotas);
+router.get('/api/obtenerUltimasCincoMontero', db.obtenerUltimasCincoMontero);
+router.get('/api/obtenerOtrasNotasMontero', db.obtenerOtrasNotasMontero);
+router.get('/api/obtenerUltimasCincoLocalidades', db.obtenerUltimasCincoLocalidades);
+router.get('/api/obtenerOtrasNotasLocalidades', db.obtenerOtrasNotasLocalidades);
+router.get('/api/obtenerUltimasCincoSantaCruz', db.obtenerUltimasCincoSantaCruz);
+router.get('/api/obtenerOtrasNotasSantaCruz', db.obtenerOtrasNotasSantaCruz);
+router.get('/api/obtenerUltimasCincoSeguridad', db.obtenerUltimasCincoSeguridad);
+router.get('/api/obtenerOtrasNotasSeguridad', db.obtenerOtrasNotasSeguridad);
+router.get('/api/obtenerUltimasCincoDeportes', db.obtenerUltimasCincoDeportes);
+router.get('/api/obtenerOtrasNotasDeportes', db.obtenerOtrasNotasDeportes);
+router.get('/api/obtenerUltimasCincoSociales', db.obtenerUltimasCincoSociales);
+router.get('/api/obtenerOtrasNotasSociales', db.obtenerOtrasNotasSociales);
+router.get('/api/obtenerUltimasCincoNacional', db.obtenerUltimasCincoNacional);
+router.get('/api/obtenerOtrasNotasNacional', db.obtenerOtrasNotasNacional);
+router.get('/api/obtenerUltimasCincoInternacional', db.obtenerUltimasCincoInternacional);
+router.get('/api/obtenerOtrasNotasInternacional', db.obtenerOtrasNotasInternacional);
 
 router.post('/api/IngresarNota', db.IngresarNota);
 router.post('/api/IngresarComentario', db.IngresarComentario);
+router.post('/api/InsertarEmpresa', db.InsertarEmpresa);
+router.post('/api/InsertarPublicidad', db.InsertarPublicidad);
 
 module.exports = router;
